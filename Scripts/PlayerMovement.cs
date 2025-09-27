5bfe14ba9f2cd6434e38e34423f44975901748a6
yuvee007 <unnattyiechandna7892@gmail.com>
2025-09-27 19:04:53 +0530

    Add files via upload

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    PlayerControls controls;
+    float direction = 0;
+
+    public float speed = 400;
+    bool isFacingRight = true;
+
+    public float jumpForce = 5;
+
+    bool isGrounded;
+    public Transform GroundCheck;
+    public LayerMask GroundLayer;
+
+
+
+    public Rigidbody2D playerRB;
+    public Animator animator;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+        controls.Enable();
+
+        controls.Land.Move.performed += ctx =>
+        {
+            direction = ctx.ReadValue<float>();
+        };
+
+        controls.Land.Jump.performed += ctx => Jump();
+    }
+
+
+    void FixedUpdate()
+    {
+      isGrounded = Physics2D.OverlapCircle(GroundCheck.position,0.1f, GroundLayer);
+      animator.SetBool("isGrounded", isGrounded);
+      
+      playerRB.linearVelocity = new Vector2(direction * speed * Time.fixedDeltaTime, playerRB.linearVelocity.y);
+      animator.SetFloat("speed", Mathf.Abs(direction));
+
+      if(isFacingRight && direction < 0  || !isFacingRight && direction>0){
+        Flip();
+
+      }
+    }
+
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+
+    }
+
+
+    void Jump()
+    {
+            playerRB.linearVelocity = new Vector2(playerRB.linearVelocity.x, jumpForce);
+    }
+}
